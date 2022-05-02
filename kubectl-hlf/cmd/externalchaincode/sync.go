d4998dccec384ab4ea3d3b45d1cf2caf953e4a32
adityajoshi12 <adityaprakashjoshi1@gmail.com>
2022-05-02 09:12:46 +0530

    removed multiple character shorthand

--- kubectl-hlf/cmd/externalchaincode/sync.go
+++ kubectl-hlf/cmd/externalchaincode/sync.go
@@ -224,6 +224,6 @@ func newExternalChaincodeSyncCmd() *cobra.Command {
 	f.BoolVarP(&c.force, "force", "", false, "Force restart of chaincode")
 	f.BoolVar(&c.tlsRequired, "tls-required", false, "Require TLS for chaincode")
 	f.IntVarP(&c.replicas, "replicas", "", 1, "Number of replicas of the chaincode")
-	f.StringArrayVarP(&c.ImagePullSecret, "image-pull-secret", "ps", []string{}, "Image Pull Secret for the Chaincode Image")
+	f.StringArrayVarP(&c.ImagePullSecret, "image-pull-secret", "s", []string{}, "Image Pull Secret for the Chaincode Image")
 	return cmd
 }
